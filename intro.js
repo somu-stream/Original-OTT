8d786d018cde0cf15cfb5a302cf77b9bedd509f0
somu-stream <somuveeraa94@gmail.com>
2026-06-30 00:11:13 +0530

    Change redirect target from auth1.html to home(1).html

--- intro.js
+++ intro.js
@@ -17,7 +17,7 @@ function startIntroRedirectLogic() {
 
     // 4.5 seconds la window replace use panni history lock panrom
     redirectTimeout = setTimeout(function() {
-        window.location.replace('auth1.html');
+        window.location.replace('home(1).html');
     }, 5000);
 
   } else {
